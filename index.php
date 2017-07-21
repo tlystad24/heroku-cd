cd4e7d789dcb0c873381f712fd5051a88044e8e2
Tobias Lystad <tobias24@me.com>
2017-07-21 19:19:07 +0200

    Update index

--- index.php
+++ index.php
@@ -2,12 +2,49 @@
 <html>
 <head>
 	<title>Heroku PHP</title>
+	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400" rel="stylesheet">
+	<style type="text/css">
+		body {
+			font-family: 'Quicksand';
+			margin: 0;
+		}
+		h1 {
+			font-weight: 300;
+			margin: 0 auto;
+			padding: 4%;
+			color: white;
+			font-size: 2rem; 
+		}
+		.heading {
+			background-image: linear-gradient( 135deg, rebeccapurple 0%, blueviolet 100%);
+			margin: 0;
+		}
+		.content {
+			width: 100%;
+			padding: 5%;
+			color: Cadetblue;
+		}
+		.date {
+			color: rebeccapurple;
+		}
+		.blockQuote {
+			border-left: 3px solid rebeccapurple;
+			padding-left: 3px; 
+		}
+		p {
+			font-weight: 400;
+		}
+	</style>
 </head>
 <body>
-	Static text
-<?php
-echo 'It\'s working!'
-?>
-
+	<div class="container">
+		<div class="heading">
+			<h1 class="headerText">Pipeline</h1>
+		</div>
+	<div class="content">
+		<p class="date">First post</p>
+		<p class="blockQuote">Testing Heroku pipelines for continous deployment</p>
+		<?php echo 'It\'s working!' ?>
+	</div>
 </body>
 </html>
